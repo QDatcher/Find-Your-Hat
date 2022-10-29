b379e52168afadb86e09cc197b8b305f6ed9db24
Quise <quentin.datcher@yahoo.com>
2022-10-29 13:27:41 -0400

    +constructor and print method need to work on bugs

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+let Field = class {
+    constructor(field){
+        this._field = field;
+    }
+
+    get field(){
+        return this._field;
+    }
+
+    print(){
+        for(let i = 0; i < this._field.length; i++){
+            this._field[i].join('')
+        }
+        console.log(this._field.join(''))
+    }
+}
+
+let field1 = new Field([
+    [fieldCharacter, hole, fieldCharacter, hat],
+    [fieldCharacter, fieldCharacter, fieldCharacter, hole]
+])
+field1.print()
